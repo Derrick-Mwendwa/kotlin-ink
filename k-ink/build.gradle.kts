87b2e152aaf1846cc58249e8d17919bece754a9e
Derek <derekmwendwa14@gmail.com>
2024-05-04 14:22:56 +0300

    Update build.gradle.kts, Update jitpack.yml, remove MANIFEST.MF

--- k-ink/build.gradle.kts
+++ k-ink/build.gradle.kts
@@ -10,51 +10,13 @@ repositories {
     mavenCentral()
 }
 
-tasks.jar {
-    manifest {
-        attributes(
-            mapOf(
-                "Implementation-Title" to project.name,
-                "Implementation-Version" to project.version
-            )
-        )
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-
-                groupId = project.group.toString()
-                artifactId = project.name
-                version = project.version.toString()
-
-                pom {
-                    name.set("Kotlin Ink")
-                    description.set("A simple ASCII art library written in Kotlin")
-                    url.set("https://github.com/Derrick-Mwendwa/KotlinInk")
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://github.com/Derrick-Mwendwa/KotlinInk/blob/main/LICENSE")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("Derrick-Mwendwa")
-                            name.set("Derrick Mwendwa")
-                            email.set("derekmwendwa14@gmail.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/Derrick-Mwendwa/KotlinInk.git")
-                        developerConnection.set("scm:git:ssh://github.com/Derrick-Mwendwa/KotlinInk.git")
-                        url.set("https://github.com/Derrick-Mwendwa/KotlinInk/tree/main")
-                    }
-                }
-            }
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
         }
     }
 }
